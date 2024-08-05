3f4f1eb3d8c2104a04b170b13c263ada7b7f3368
harshavdhan <139610858+harshavdhan@users.noreply.github.com>
2024-08-05 23:36:37 +0530

    Inserted Files Harsh

---
+++ test.py
@@ -0,0 +1,12 @@
+import unittest
+from main import to_upper
+
+
+class MytestCase(unittest.TestCase):
+    def test_to_upper(self):
+        name="Harsh"
+        upper_name=to_upper(name)
+        self.assertEqual(upper_name,'HARSH')
+       
+if __name__ == "__main__":
+    unittest.main()
